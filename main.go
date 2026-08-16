e77107a24b6b96afe5698b40dbc1b68ea3ad54ba
Naufal Bagaskara <naufal.bagaskara-2024@vokasi.unair.ac.id>
2026-08-16 23:31:38 +0700

    First commit, add: go.sum, go.mod, & main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("halo bapak!")
+	})
+	log.Fatal(app.Listen(":8080"))
+}
