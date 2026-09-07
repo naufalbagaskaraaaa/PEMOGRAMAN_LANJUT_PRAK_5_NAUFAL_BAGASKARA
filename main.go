02cd0ecba23cd20a32250f727b1239173a2dbab2
Naufal Bagaskara <naufal.bagaskara-2024@vokasi.unair.ac.id>
2026-09-07 07:17:51 +0700

    update main.go

--- main.go
+++ main.go
@@ -1,14 +1,47 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+
+	"latihan-fiber/app/repository"
+	"latihan-fiber/app/service"
+	"latihan-fiber/config"
+	"latihan-fiber/database"
+	appMiddleware "latihan-fiber/middleware"
+	"latihan-fiber/route"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("halo bapak!")
+	if err := config.LoadDotEnv(); err != nil {
+		log.Fatalf("memuat .env: %v", err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := database.NewPostgresPool(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
+	studentRepo := repository.NewStudentRepository(pool)
+	studentService := service.NewStudentService(studentRepo)
+
+	app := fiber.New(fiber.Config{
+		AppName:      "Tugas Mandiri REST API - Student Management",
+		ErrorHandler: appMiddleware.ErrorHandler,
 	})
-	log.Fatal(app.Listen(":8080"))
+
+	appMiddleware.Register(app)
+
+	route.Register(app, pool, studentService)
+
+	app.Use(appMiddleware.NotFound)
+
+	fmt.Println("Server running at http://localhost:3000")
+	log.Fatal(app.Listen(":3000"))
 }
