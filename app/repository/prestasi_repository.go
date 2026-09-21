48d2d7944ebe87fb5a25dc247d903d56ef589942
Naufal Bagaskara <naufal.bagaskara-2024@vokasi.unair.ac.id>
2026-09-21 08:28:10 +0700

    add repo prestasi/tantangan

---
+++ app/repository/prestasi_repository.go
@@ -0,0 +1,91 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"latihan-fiber/app/model"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var ErrPrestasiNotFound = errors.New("prestasi tidak ditemukan")
+
+type PrestasiRepository interface {
+	FindByID(ctx context.Context, idPrestasi string) (model.Prestasi, error)
+	Create(ctx context.Context, req model.CreatePrestasiRequest) (model.Prestasi, error)
+}
+
+type prestasiPostgresRepository struct {
+	pool *pgxpool.Pool
+}
+
+func NewPrestasiRepository(pool *pgxpool.Pool) PrestasiRepository {
+	return &prestasiPostgresRepository{pool: pool}
+}
+
+func (r *prestasiPostgresRepository) Create(
+	ctx context.Context,
+	req model.CreatePrestasiRequest,
+) (model.Prestasi, error) {
+	var prestasi model.Prestasi
+	err := r.pool.QueryRow(ctx, `
+		INSERT INTO prestasi (id_prestasi, nama_prestasi, juara, nim)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id_prestasi, nama_prestasi, juara, nim
+	`, req.IDPrestasi, req.NamaPrestasi, req.Juara, req.NIM).Scan(
+		&prestasi.IDPrestasi,
+		&prestasi.NamaPrestasi,
+		&prestasi.Juara,
+		&prestasi.NIM,
+	)
+	if err != nil {
+		return model.Prestasi{}, err
+	}
+	prestasi.NamaPrestasi = strings.TrimSpace(prestasi.NamaPrestasi)
+	return prestasi, nil
+}
+
+func (r *prestasiPostgresRepository) FindByID(
+	ctx context.Context,
+	idPrestasi string,
+) (model.Prestasi, error) {
+	var prestasi model.Prestasi
+	var namaPrestasi string
+	var namaMahasiswa *string
+
+	query := `
+        SELECT
+            p.id_prestasi,
+            p.nama_prestasi,
+            p.juara,
+            p.nim,
+            s.name
+        FROM prestasi p
+        LEFT JOIN students s ON s.nim = p.nim
+        WHERE p.id_prestasi = $1
+    `
+
+	err := r.pool.QueryRow(ctx, query, idPrestasi).Scan(
+		&prestasi.IDPrestasi,
+		&namaPrestasi,
+		&prestasi.Juara,
+		&prestasi.NIM,
+		&namaMahasiswa,
+	)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return model.Prestasi{}, ErrPrestasiNotFound
+		}
+
+		return model.Prestasi{}, err
+	}
+
+	prestasi.NamaPrestasi = strings.TrimSpace(namaPrestasi)
+	prestasi.NamaMhs = namaMahasiswa
+
+	return prestasi, nil
+}
