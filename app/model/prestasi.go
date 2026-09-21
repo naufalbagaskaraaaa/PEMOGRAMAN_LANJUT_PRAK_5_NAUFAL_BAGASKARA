d5d5062c93b41396ed20af475ef697e95fdf799a
Naufal Bagaskara <naufal.bagaskara-2024@vokasi.unair.ac.id>
2026-09-21 08:27:09 +0700

    update prestasi.go/tantangan

--- app/model/prestasi.go
+++ app/model/prestasi.go
@@ -1,8 +1,16 @@
 package model
 
 type Prestasi struct {
-	IDPrestasi   string `json:"id"`
-	NamaPrestasi string `json:"nama_prestasi"`
-	Juara        int    `json:"juara"`
-	NIM          int    `json:"nim"`
+	IDPrestasi   string  `json:"id_prestasi"`
+	NamaPrestasi string  `json:"nama_prestasi"`
+	Juara        *int    `json:"juara,omitempty"`
+	NIM          *string `json:"nim,omitempty"`
+	NamaMhs      *string `json:"nama_mahasiswa,omitempty"`
+}
+
+type CreatePrestasiRequest struct {
+	IDPrestasi   string  `json:"id_prestasi"`
+	NamaPrestasi string  `json:"nama_prestasi"`
+	Juara        *int    `json:"juara"`
+	NIM          *string `json:"nim"`
 }
